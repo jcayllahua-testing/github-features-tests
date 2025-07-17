f9dfafabd180d30124fb20c0986875f74c00d472
JCayllahuaG <u202019558@upc.edu.pe>
2025-07-17 10:55:04 -0500

    initialize project with Spring Boot and Gradle setup

---
+++ src/main/java/com/test/testfeaturesfromgithub/TestFeaturesFromGithubApplication.java
@@ -0,0 +1,13 @@
+package com.test.testfeaturesfromgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFeaturesFromGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestFeaturesFromGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/testfeaturesfromgithub/TestFeaturesFromGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testfeaturesfromgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFeaturesFromGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
